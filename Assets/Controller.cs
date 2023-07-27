02739142d9fe8cef34f96f0298944ba817c8a2db
mingyu0304 <mingyu030304@gmail.com>
2023-07-27 19:45:40 +0900

    ??

---
+++ Assets/Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator anim;
+    private bool isAnimationFinished = false;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+
+    }
+}
